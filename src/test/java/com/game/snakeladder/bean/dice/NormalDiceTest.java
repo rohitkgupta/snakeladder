9caaa2de1e443540352140acb717162a66e5064c
Rohit Gupta <rohit.gupta@vuclip.com>
2020-10-21 19:24:02 +0530

    added test cases for dice

---
+++ src/test/java/com/game/snakeladder/bean/dice/NormalDiceTest.java
@@ -0,0 +1,37 @@
+package com.game.snakeladder.bean.dice;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class NormalDiceTest {
+    private Dice dice;
+
+    @Before
+    public void setup() {
+        dice = new CrookedDice();
+    }
+
+    @Test
+    public void testRoll() {
+        int diceValue = dice.roll();
+        validateDiceValue(diceValue);
+    }
+
+    private void validateDiceValue(int diceValue) {
+        Assert.assertTrue(diceValue < 7);
+        Assert.assertTrue(diceValue > 0);
+    }
+
+    @Test
+    public void testRoll1() {
+        int diceValue = dice.roll();
+        validateDiceValue(diceValue);
+    }
+
+    @Test
+    public void testRoll2() {
+        int diceValue = dice.roll();
+        validateDiceValue(diceValue);
+    }
+}
